be3976c3c8ac368c44ff131be3a560ffa38ef20e
facundolgomez <facundolgomez87@gmail.com>
2025-11-06 01:01:58 -0300

    first commit

---
+++ statemusic/statemusic.py
@@ -0,0 +1,11 @@
+
+menu_active = True
+
+while menu_active:
+	print("Welcome")
+	user_input = int(input("Ingrese un numero: "))
+	if user_input == 2:
+		menu_active = False
+
+print("Terminó el programa")
+
