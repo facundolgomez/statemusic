adffc658452b2c5cecd6be96bb8ffa5af6667988
facundolgomez <facundolgomez87@gmail.com>
2025-11-15 15:56:25 -0300

    implement OAuth method

--- statemusic/statemusic.py
+++ statemusic/statemusic.py
@@ -1,6 +1,9 @@
 import requests
 import os
+import webbrowser
 from dotenv import load_dotenv
+from urllib.parse import urlencode
+import time
 
 
 load_dotenv(dotenv_path="credentials.env")
@@ -26,6 +29,33 @@ def get_token():
 	TOKEN = result.get("access_token")
 
 
-get_token()
+def get_authorization():
+	
+    
+    params = {
+		"response_type": 'code',
+		"client_id": os.getenv("SPOTIFY_CLIENT_ID"),
+		"redirect_uri": "http://127.0.0.1:8888/callback",
+		"state": "state"
+	}
+    url = "https://accounts.spotify.com/authorize?" + urlencode(params)
+    
+    
+   
+    webbrowser.open(url)
+    while not os.path.exists("code.txt"):
+        time.sleep(1)
+    
+    with open("code.txt", "r") as f:
+        code = f.read().strip()	
+    
 
-print(TOKEN)
+    
+
+if __name__ == "__main__":
+	get_token()
+	get_authorization()
+	
+	
+	
+
